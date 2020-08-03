388e7914e13c34051e349c47d722b8d4b778660a
Leandro Ikehara <leandro.ikehara@gmail.com>
2020-08-03 13:26:11 -0300

    Update Heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -101,8 +101,9 @@ function CadastroCategoria() {
 
       <ul>
         {categorias.map((categoria, indice) => (
+          // eslint-disable-next-line react/no-array-index-key
           <li key={`${categoria}${indice}`}>
-            {categoria.nome}
+            {categoria.titulo}
           </li>
         ))}
       </ul>
