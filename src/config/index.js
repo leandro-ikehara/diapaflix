7df0331ad24d3675f48b4d3b1288f605ddd22c12
Leandro Ikehara <leandro.ikehara@gmail.com>
2020-08-09 23:56:39 -0300

    Último commit, espero...

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://diapaflix.herokuapp.com/';
+  : 'https://diapaflix.herokuapp.com';
 
 export default {
   URL_BACKEND,
